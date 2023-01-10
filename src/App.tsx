9bcdf0283e4053a65f74dbfac5607d0b3fdb10ef
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2023-01-10 23:43:39 +0200

    added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>Articles will be here</h1>}>
+          <Route
+            path=":articleId"
+            element={<h1>Articles content will be here</h1>}
+          />
+        </Route>
+
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
     </div>
   );
 }
