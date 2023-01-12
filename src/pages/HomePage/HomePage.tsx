0f5cb5478743cea4d67c3f34fb8834d0a04e7be3
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2023-01-12 02:00:58 +0200

    implemented responsive layout for home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+
+import './HomePage.scss';
+
+import { Typography, Box, OutlinedInput, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { useAppSelector } from '../../app/hooks';
 import { ArticlesList } from '../../features/articles/ArticlesList';
 
 export const HomePage: React.FC = () => {
-  return <ArticlesList />;
+  const { articles, loading, error } = useAppSelector(
+    (state) => state.articles
+  );
+
+  return (
+    <Box component="main" className="homePage">
+      <Typography className="homePage__search">Filter by keywords</Typography>
+      <OutlinedInput
+        className="homePage__search_input"
+        startAdornment={
+          <InputAdornment position="start">
+            <SearchIcon className="homePage__search_icon" />
+          </InputAdornment>
+        }
+      />
+      <Typography className="homePage__count">{`Results: ${articles.length}`}</Typography>
+      <ArticlesList />
+    </Box>
+  );
 };
